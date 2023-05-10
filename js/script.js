95cc60946b09ea9c23c1becb0de9d1640db29606
osunamartin <martin.osuna13@gmail.com>
2023-05-10 19:26:16 -0300

    Forms en CONTACTO, actualización de index

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function validarFormulario1() {
+  var nombre = document.forms["formulario1"]["nombre"].value;
+  if (nombre == "") {
+    alert("Por favor, ingresa un nombre válido.");
+    return false;
+  
+  }
+  /* if (!isNaN(nombre)) {
+    alert("Por favor, ingresa un nombre válido (no se permiten números).");
+    return false;
+  } 
+    VER COMO HACER PARA QUE NO ME DEJE PONER NÚMEROS EN EL NOMBRE  
+  */ 
+}
+
+function validarFormulario2() {
+  var email = document.forms["formulario2"]["email"].value;
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email == "") {
+    alert("Por favor, ingresa una dirección de correo electrónico.");
+    return false;
+  }
+
+  if (!email.match(emailRegex)) {
+    alert("Por favor, ingresa una dirección de correo electrónico válida.");
+    return false;
+  }
+}
