b4b08e81a4e65ae936ca43241de24ef7f096a168
el-guey <gonzalez.nestoremmanuel@gmail.com>
2023-05-24 13:32:01 -0300

    Validación del form con js, responsive API Clima

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
-// FORMULARIO + VALIDACIÓN 
+//Const
+const nombre = document.getElementById("name");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const error = document.getElementById("error");
+const formulario = document.getElementById("form");
 
-function validarFormulario1() {
-  var nombre = document.forms["formulario1"]["nombre"].value;
-  if (nombre == "") {
-    alert("Por favor, ingresa un nombre válido.");
-    return false;
-  
-  }
-  /* if (!isNaN(nombre)) {
-    alert("Por favor, ingresa un nombre válido (no se permiten números).");
-    return false;
-  } 
-    VER COMO HACER PARA QUE NO ME DEJE PONER NÚMEROS EN EL NOMBRE  
-  */ 
-}
+//Regular expresión
+let regexLetras = /^[A-Za-z]+$/;  
+let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
-function validarFormulario2() {
-  var email = document.forms["formulario2"]["email"].value;
-  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//Variables
+let msjAlerta="";
+let alerta =false;
 
-  if (email == "") {
-    alert("Por favor, ingresa una dirección de correo electrónico.");
-    return false;
-  }
-
-  if (!email.match(emailRegex)) {
-    alert("Por favor, ingresa una dirección de correo electrónico válida.");
-    return false;
+//Functions
+//Valid name, lastname
+function validText(campo, valor){
+  if(valor.length < 4 || !regexLetras.test(valor)){
+    msjAlerta += "Por favor introduce un "+ campo.name +" válido. Al menos 4 letras sin números, espacios o caracteres especiales. <br>";
+    alerta =true;
   }
 }
-// Traer los labels para las func
-function validarFormulario1(){
-  validarFormulario1();
-  validarFormulario2();
+//Valid email
+function validEmail(){
+  if(!regexEmail.test(email.value) || email.value.length < 6){
+    msjAlerta += "Por favor introduce un E-mail válido <br>";
+    alerta =true;
+  }
+}
+//Valid Form
+function validForm(){
+  validText(nombre,nombre.value);
+  validText(apellido,apellido.value);
+  validEmail();
 }
 
-
-
+//Event on form submit
+formulario.addEventListener("submit",e=>{
+  msjAlerta=""
+  validForm();
+  if (alerta){
+    e.preventDefault();
+    error.innerHTML = msjAlerta;
+  }
+  else{
+    msjAlerta ="";
+  }
+})
